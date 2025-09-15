965cf57cb2bbb07863217aea381c10c3f7e7c826
03conner <03conner.chan@kamsc.org>
2025-09-15 13:50:33 -0400

    sdfdsf

--- src/main/java/SumProductAverage.java
+++ src/main/java/SumProductAverage.java
@@ -17,6 +17,37 @@ import java.util.Scanner;
 
 public class SumProductAverage {
     public static void main(String[] args) {
-        // TODO
+        // NAME                 :   Conner Chan
+// GROUP                :   APCSA
+// LAST MODIFIED        :   9/9/25
+// PROBLEM ID           :   2.21 - 2.22
+// PROBLEM DESCRIPTION  :   Have the user imput # numbers then output
+//                      :   the average, difference, and the sum
+// SOURCES USED         :   AP CS textbook
+// PEOPLE I HELPED      :   Michael Xndzov
+// I RECEIVED HELP FROM :   Michael Xendzov
+        // declares varables
+        double num1, num2, num3, num4, numruns;
+        // prompt the uers for the number of runs
+        Scanner scan = new Scanner(System.in);
+        System.out.println("How many runs?");
+        numruns = scan.nextDouble();
+        num2 = 0;
+        num3 = 1;
+        for (int k = 0; k < numruns; k++) {
+            System.out.println("Enter 1 number");
+            num1 = scan.nextDouble();
+            num2 = num1 + num2;
+            num3 = num1 * num3;
+        }
+        num4 = num2 / numruns;
+        // outputs the sum, product, and average
+        System.out.println("The sum is " + num2);
+        System.out.println("The product is " + num3);
+        System.out.println("The average is " + num4);
+
+
     }
 }
+
+
